08708d4b76a72b2e1061a6e591e660f1bf90bafa
Ratul1997 <42713519+Ratul1997@users.noreply.github.com>
2021-08-07 23:07:03 +0600

    Create index.js
    Change This

--- BackEnd/server/index.js
+++ BackEnd/server/index.js
@@ -4,14 +4,22 @@ const mysql = require('mysql')
 const axios = require('axios').create({
     baseUrl : "https://jsonplaceholder.typicode.com/"
 })
+
+
+///make it in FOlders///
 const cors = require('cors')
 
+
+///env file
 const PORT = 9001
 const app = express();
 
 /// establishing connection
 
+
+
 const db = mysql.createConnection({
+    ///env file
     connectionLimit : 100,
     host : 'localhost',
     user : 'root',
@@ -45,7 +53,7 @@ app.get("/api" , (req, res) =>{
     })
 })
 ////  test run ends
-app.post('/signup' , (req , res) =>{
+app.post('/signup',(req , res) =>{
     const userHandleName = req.body.newUserDetails.userHandleName;
     const userPassword = req.body.newUserDetails.userPassword; 
     const userName = req.body.newUserDetails.userName;
@@ -53,9 +61,9 @@ app.post('/signup' , (req , res) =>{
     const userPhoneNumber = req.body.newUserDetails.userPhoneNumber;
     const userInsertedTime = req.body.newUserDetails.userInsertedTime;
     const userUpdatedTime = req.body.newUserDetails.userUpdatedTime;
-    const sqlInstert = "INSERT INTO users (userId, userHandle, userPassword, userName, userPhoneNumber, userInsertedTime, userUpdatedTime , userEmailAddress) VALUES (?,?,?,?,?,?,?,?)";
+    const sqlInstert = "INSERT INTO users ( userHandle, userPassword, userName, userPhoneNumber, userInsertedTime, userUpdatedTime , userEmailAddress) VALUES (?,?,?,?,?,?,?,?)";
     if(userHandleName){
-        db.query(sqlInstert , ["NULL",userHandleName,userPassword,userName,userPhoneNumber,userInsertedTime,userUpdatedTime,userEmailAddress] , (error,result)=>{
+        db.query(sqlInstert , [userHandleName,userPassword,userName,userPhoneNumber,userInsertedTime,userUpdatedTime,userEmailAddress] , (error,result)=>{
             if(error){
                 console.log(error);
             }else{
@@ -71,11 +79,15 @@ app.post('/login',(req,res) =>{
     const userEmailAddress = req.body.tempUserDetails.tempUserEmail;
     const userPassword = req.body.tempUserDetails.tempUserPassword;
     
+    
+    ///Qurery Diffrent folder
     db.query(`SELECT * FROM users where userHandle = '${userHandleName}' and userEmailAddress = '${userEmailAddress}'
         and userPassword = '${userPassword}'`, (error,result) =>{
             if(error){
                 console.log(error);
             } else {
+                
+                /// response status code///
                 res.send(result);
                 console.log(result);
             }
@@ -89,14 +101,22 @@ app.post('/postNewElements',(req,res) =>{
     const postUpdatedTime = req.body.tempNewPost.postUpdatedTime;
     const postUserName = req.body.tempNewPost.postUserName;
     console.log(req.body.tempNewPost);
-    const sqlInstert = "INSERT INTO posts (postId, userId, postDescription, postInsertedTime, postUpdatedTime, postUserName) VALUES (?, ?, ?, ?, ?,?)";
+    const sqlInstert = "INSERT INTO posts (userId, postDescription, postInsertedTime, postUpdatedTime, postUserName) VALUES (?, ?, ?, ?, ?,?)";
+    const postData = [userId,postDescription,postInsertedTime,postUpdatedTime,postUserName.noOfLikes]
     if(userId){
-        db.query(sqlInstert, ["NULL" , userId,postDescription,postInsertedTime,postUpdatedTime,postUserName.noOfLikes],(error,result) =>{
+        db.query(sqlInstert, postData ,(error,result) =>{
             if(error){
+                /// response for error
+                res.status(500).json({msg:error.message});
                 console.log(error);
             } else {
                 console.log(result);
-                res.send(result)
+                
+                const jsonObject = {
+                    total_post: result.length,
+                    posts:[...result]
+                }
+                res.status(201).json(jsonObject)
             }
         })
     }
@@ -144,12 +164,19 @@ app.post("/userInfo",(req,res) =>{
 {/**updating user info  */}
 app.post("/updateUserInfo",(req,res) =>{
     const userId = req.body.updateUserDetails.userId;
-    const userHandleName = req.body.updateUserDetails.userHandleName;
-    const userName = req.body.updateUserDetails.userName;
-    const userEmailAddress = req.body.updateUserDetails.userEmailAddress;
-    const userPhoneNumber = req.body.updateUserDetails.userPhoneNumber;
-    const userPassword = req.body.updateUserDetails.userPassword;
-    const userUpdatedTime = req.body.updateUserDetails.userUpdatedTime;
+    
+    if(userId === null) res.status(401).json({msg:'Unauthorized'})
+    
+    const currentTime = new Date()
+    
+    
+    const {updateUserDetails }= req.body
+    const userHandleName = updateUserDetails.userHandleName || null;
+    const userName = updateUserDetails.userName || null ;
+    const userEmailAddress = updateUserDetails.userEmailAddress;
+    const userPhoneNumber = updateUserDetails.userPhoneNumber;
+    const userPassword = updateUserDetails.userPassword;
+    const userUpdatedTime = updateUserDetails.userUpdatedTime || currentTime;
     db.query(`UPDATE users SET userHandle='${userHandleName}', userPassword='${userPassword}', userName='${userName}', userPhoneNumber='${userPhoneNumber}', userUpdatedTime='${userUpdatedTime}', userEmailAddress='${userEmailAddress}' WHERE userId = '${userId}'`,(error,rows) =>{
         if(error){
             console.log(error);
